ed42911ed14885e6bbeb1c56da2a53fa8317be87
Evgeny Ralko <yauheni_ralko@epam.com>
2018-04-09 18:05:21 +0300

    Initial Setup: added react, react-dom

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const config = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ]
+  }
+}
+
+module.exports = config;
