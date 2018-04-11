66555a37ec84bec222264d6722b1b4fc84f72fa4
Evgeny Ralko <yauheni_ralko@epam.com>
2018-04-11 17:08:58 +0300

    Task3: initial commit

--- src/app/components/TCreateElement/TCreateElement.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TCreateElement = () => React.createElement(
-  'div',
-  null,
-  ['Create Element component'],
-);
-
-export default TCreateElement;
--- src/app/components/TFunctionalComponent/TFunctionalComponent.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const TFunctionalComponent = () => <div>Functional Component</div>;
-
-export default TFunctionalComponent;
